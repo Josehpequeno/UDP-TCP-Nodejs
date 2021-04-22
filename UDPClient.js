d9a5b6273d4060822a0495e30e3687d2e0e237ea
josehpequeno <hicarojbs21@gmail.com>
2021-04-22 20:53:16 -0300

    readline, UDP

--- UDPClient.js
+++ UDPClient.js
@@ -1,12 +1,35 @@
 var PORT = 33333;
 var HOST = '127.0.0.1';
 
-var dgram = require('dgram');
-var message = 'Hi marcus, you are so beautiful!';
-
-var client = dgram.createSocket('udp4');
-client.send(message, 0, message.length, PORT, HOST, function(err, bytes) {
-  if (err) throw err;
-  console.log('UDP message sent to ' + HOST +':'+ PORT);
-  client.close();
+const dgram = require('dgram');
+const client = dgram.createSocket('udp4');
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
 });
+//var message = 'Hi marcus, you are so beautiful!';
+rl.question("What is your name ? ", function (name) {
+    rl.question("What is the message ? ", function (msg) {
+        //console.log(`${name}, is a citizen of ${country}`);
+        var message = `${name} said ${msg}`;
+        client.send(message, 0, message.length, PORT, HOST, function (err, bytes) {
+            if (err) throw err;
+            console.log('UDP message sent to ' + HOST + ':' + PORT);
+            client.close();
+            rl.close();
+        });
+    });
+});
+
+rl.on('SIGINT', function () {
+    console.log("Caught interrupt signal");
+
+    //if (i_should_exit)
+    process.exit();
+});
+
+rl.on("close", function () {
+    console.log("\nMessage sent \nBYE BYE !!nt = dgram.createSocket('udp4');
+});
+
