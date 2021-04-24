aa2f181f0fd664cbf4aa0792db9efa153c23726e
Hícaro José Barbosa Soares <57676255+Josehpequeno@users.noreply.github.com>
2021-04-23 22:00:01 -0300

    Update UDPServer.js

--- UDPServer.js
+++ UDPServer.js
@@ -2,6 +2,7 @@ var PORT = 33333;
 var HOST = '127.0.0.1';
 //app.set("HOST", process.env.HOST || "localhost");
 console.log("host: ",process.env.HOST)
+console.log("host: ","localhost")
 
 var dgram = require('dgram');
 var server = dgram.createSocket('udp4');
